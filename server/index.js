16feebb5e5b1c1a16929ca094e3e363e41b5d010
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-09 21:53:13 -0800

    movie list GET endpoint

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
-
 // import express
 const express = require("express");
+const movies = require("./movies.json");
 
 // create express app
-const app = express()
+const app = express();
 
+// root page
 app.get("/", (req, res) => {
-    return res.send("HELLO WORLD!")
-})
+  return res.send("HELLO WORLD!");
+});
+
+// movie list page
+app.get("/movies/list", (req, res) => {
+  return res.send(movies);
+});
 
 // listen on port
 app.listen(8080, () => {
-    console.log("Now listening on PORT 8080")
-})
-
+  console.log("Now listening on PORT 8080");
+});
