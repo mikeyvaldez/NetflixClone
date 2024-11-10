1ed3d03112cde2f7b31353718c70c1c9d9b2ded5
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-10 12:14:32 -0800

    fixed card styles and navbar background

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 
 
 const tabs = ["Home", "Series", "Films", "New & Popular", "My List", "Browse by Languages"]
 
 export default function NavBar() {
+
+  const [showBackground, setShowBackground] = useState(false);
+
+  // when scrolling down make navbar turn black and opaic
+  useEffect(() => {
+    window.addEventListener("scroll", () =>{
+      if(window.scrollY > 400){
+        setShowBackground(true)
+      } else {
+        setShowBackground(false)
+      }
+    })
+  }, [])
+
   return (
     <nav className="w-full fixed z-40">
-      <div className="px-16 py-6 flex items-center">
+      <div className={`px-16 py-6 flex items-center ${showBackground ? 'bg-black bg-opacity-90' : null}`}>
         <img
           className="h-16"
           src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets.stickpng.com%2Fimages%2F580b57fcd9996e24bc43c529.png&f=1&nofb=1&ipt=5a57e6f95affd6c6ff40e7a1db2400223a8f4edc8ef2054243701ddccec2b3bf&ipo=images"
