3bac6ccf560869f22aa13196b45f841e26e23956
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-18 11:06:23 -0800

    the sign in endpoint

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const { check, validationResult } = require("express-validator");
 const { prisma } = require("../db");
-const bcrypt = require("bcrypt")
-const JWT = require("jsonwebtoken")
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
+const { json } = require("express");
 
+// this post route creates a new user:
 router.post(
+  // validate input
   "/signup",
   [
     check("email", "Please input a valid email").isEmail(),
@@ -34,14 +37,17 @@ router.post(
       },
     });
 
+    // validate that the user doesn't already exist
     if (user) {
       return res.status(400).json({
         errors: [{ msg: "This user already exists" }],
       });
     }
 
+    // hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    // save the user
     const newUser = await prisma.user.create({
       data: {
         email,
@@ -52,18 +58,65 @@ router.post(
         id: true,
         username: true,
         email: true,
-      }
-    })
+      },
+    });
 
     // create json web token and referncing environment variable for security purposes
-    const token = await JWT.sign(newUser, process.env.JSON_WEB_TOKEN_SECRET, {expiresIn: 3600000})
+    const token = await JWT.sign(newUser, process.env.JSON_WEB_TOKEN_SECRET, {
+      expiresIn: 3600000,
+    });
 
-    res.json({
+    // return JWT
+    return res.json({
       user: newUser,
       token,
     });
-    
   }
 );
+
+// validate that the user exists
+// validate the password
+// rturn a JWT
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      errors: [{ msg: "Ivalid credentials" }],
+    });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.status(400).json({
+      errors: [{ msg: "Ivalid credentials" }],
+    });
+  }
+
+  const userPayload = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  const token = await JWT.sign(
+    userPayload,
+    process.env.JSON_WEB_TOKEN_SECRET,
+    { expiresIn: 3600000 }
+  );
+
+  // return JWT
+  return res.json({
+    user: userPayload,
+    token,
+  });
+});
 
 module.exports = router;
