4f53991e91f2b1964136420b1a6c2be7cfe395a2
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-20 12:16:48 -0800

    connecting our server to stripe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import "./App.css";
 import useAuth from "./hooks/useAuth";
 import { useEffect } from "react";
 
---
+++ server/utils/stripe.js
@@ -0,0 +1,9 @@
+const Stripe = require("stripe");      // require stripe
+
+// create instance of stripe
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2024-10-28"
+})    
+
+
+module.exports = { stripe };
