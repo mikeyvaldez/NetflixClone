303439d7e56f6139066a9091db21ed0f28b56940
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-21 10:48:54 -0800

    defining a session endpoint

--- server/routes/sub.js
+++ server/routes/sub.js
@@ -3,30 +3,50 @@
 //------------------------------
 
 // require express router
-const router = require("express").Router()
+const router = require("express").Router();
 const { stripe } = require("../utils/stripe");
-
+// const checkAuth = require("../middleware");
 
 // logic to fetch our products
+
 router.get("/products", async (req, res) => {
-    const response = await stripe.products.list({        
-        expand: ["data.default_price"]
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = response.data.map(({ id, name, default_price }) => {
+    return {
+      id,
+      name,
+      canDownload: true,
+      canWatchTPB: name === "Premium Plan" ? true : false,
+      price: {
+        amount: default_price.unit_amount,
+        id: default_price.id,
+      },
+    };
+  });
+
+  return res.json(products);
+});
+
+router.post("/session", async (req, res) => {
+    const { priceId, email } = req.body;
+    const session = await stripe.checkout.sessions.create({
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      success_url: "http://localhost:5173/browse",
+      cancel_url: "http://localhost:5173/plans",
+      customer_email: email,
     });
-
-    const products = response.data.map(({ id, name, default_price }) => {
-        return {
-            id,
-            name,
-            canDownload: true,
-            canWatchTPB: name === "Premium Plan" ? true : false,
-            price: {
-                amount: default_price.unit_amount,
-                id: default_price.id,
-            }
-        }
-    })
-
-    return res.json(products);
-})
+  
+    return res.json(session);
+  });
 
 module.exports = router;
