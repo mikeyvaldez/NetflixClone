6af3bab7c9278608be2bfee47640749e4122db8a
Michael Valdez <mikeyvaldez92@yahoo.com>
2024-11-23 18:15:08 -0800

    pre installations

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");  // import express
 const cors = require("cors");        // import cors
 const app = express();  // create express app
+const path = require("path");
 
 app.use(express.json())
 app.use(cors());
@@ -17,8 +18,15 @@ app.use("/auth", require("./routes/auth"));
 app.use("/sub", require("./routes/sub"));
 
 const port = process.env.PORT;
+const __dirname = path.resolve();
 
 // listen on port
 app.listen(port, () => {
   console.log(`Now listening on PORT ${port}`);
 });
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
+})
